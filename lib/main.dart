163778fa7802e0fdb44dd551779f4962be940ede
QILIN <sengheng.measheanh@gmail.com>
2023-12-19 12:03:57 +0700

    adding cross-screen navigation and passing data to target screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const CategoriesScreen()
     );
   }
 }
